476c71921bf7c8b695c12ed75667a7c733406137
stonebrave <situjiuxiang@163.com>
2022-05-08 19:20:57 +0800

    2.5

--- src/test/java/com/itheima/Chapter02ApplicationTests.java
+++ src/test/java/com/itheima/Chapter02ApplicationTests.java
@@ -4,6 +4,7 @@ import com.itheima.domain.Example;
 import com.itheima.domain.MyProperties;
 import com.itheima.domain.Person;
 import com.itheima.domain.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,13 +37,20 @@ class Chapter02ApplicationTests {
     private MyProperties myProperties;
     @Test
     public void myPropertiesTest() {
-        System.out.println(person);
+        System.out.println(myProperties);
     }
 
     @Autowired
     private ApplicationContext applicationContext;
     @Test
     public void iocTest(){
-        System.out.println(applicationContext.containsBean("person"));
+        System.out.println(applicationContext.containsBean("myService"));
+    }
+
+    @Value(("${tom.description}"))
+    private String description;
+    @Test
+    public void placeholderTest() {
+        System.out.println(description);
     }
 }
